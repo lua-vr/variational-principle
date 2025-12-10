9169a8ed67ab887efe2d6a0757ae5d878a0d3bd0
Lua Viana Reis <me@lua.blog.br>
2025-12-10 19:41:10 -0300

    blueprint: improve watch script

--- blueprint/watch.sh
+++ blueprint/watch.sh
@@ -1,2 +1,5 @@
-while inotifywait -r -e close_write .; do lake exe blueprint; done &
-(cd _out/html-multi/ && uvx reloadserver)
+while inotifywait -r --exclude "./_out/" -e close_write .; do
+    lake exe blueprint;
+    curl -X POST localhost:8000/api-reloadserver/trigger-reload;
+done &
+(cd _out/html-multi/ && uvx reloadserver -w)
